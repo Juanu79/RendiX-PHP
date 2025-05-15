6a875efcafe020bb39ceb4b75d2d0f49b6c19680
Carlos3540 <carlosestecnologia@gmail.com>
2025-05-14 22:01:35 -0500

    Resuelto conflicto de merge en archivos JS

--- views/JS/iniciarsesion.js
+++ views/JS/iniciarsesion.js
@@ -1,6 +1,6 @@
 function iniciarSesion(event) {
     event.preventDefault();//evita el envio del formulario
-<<<<<<< HEAD
+
 window.location.href="views/index.phpl"; //Link de la pagina principal
 }
 
@@ -10,15 +10,4 @@ function Iniciar(event) {
 
 function Registrarse(event) {
     window.location.href="HTML/registrarse.php"; //Link de la pagina principal
-=======
-window.location.href="../index.php"; //Link de la pagina principal
-}
-
-function Iniciar(event) {
-    window.location.href="../index.php"; //Link de la pagina principal
-}
-
-function Registrarse(event) {
-    window.location.href="registrarse.php"; //Link de la pagina principal
->>>>>>> f1476c5def3b994c7317432c1d1ace7338412c98
 }
